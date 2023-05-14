77929041f0d271daef21f11364dbaca1fda1f425
John-Thailand <hamoban69@gmail.com>
2023-05-14 21:08:00 +0900

    ユーザー作成フォームとAPIを繋げよう

--- app/public/js/index.js
+++ app/public/js/index.js
@@ -1,8 +1,24 @@
 const indexModule = (() => {
-    // 検索ボタンをクリックした時のイベントリスナー設定
-    document.getElementById('search-btn').addEventListener('click', () => {
-        return searchModule.searchUsers()
-    })
-    // UsersモジュールのfetchAllUsersを呼び出す
-    return usersModule.fetchAllUsers()
+    const path = window.location.pathname
+
+    switch (path) {
+        case '/':
+            // 検索ボタンをクリックした時のイベントリスナー設定
+            document.getElementById('search-btn').addEventListener('click', () => {
+                return searchModule.searchUsers()
+            })
+            // UsersモジュールのfetchAllUsersを呼び出す
+            return usersModule.fetchAllUsers()
+        case '/create.html':
+            document.getElementById('save-btn').addEventListener('click', () => {
+                return usersModule.createUser()
+            })
+            document.getElementById('cancel-btn').addEventListener('click', () => {
+                return window.location.href = '/'
+            })
+            break;
+        
+        default: 
+            break;
+    }
 }) ()
